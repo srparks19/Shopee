25f82dfec09bb406034d8d6c1da46246deaaa357
Sebastian Pérez López <shanlonp4@gmail.com>
2021-10-09 21:44:12 -0500

    blog

--- Template/_blogs.php
+++ Template/_blogs.php
@@ -2,32 +2,19 @@
 <!-- Blogs -->
 <section id="blogs">
     <div class="container py-4">
-        <h4 class="font-rubik font-size-20">Ultimos blogs</h4>
+        <h4 class="font-rubik font-size-20">Ultimos blogs 2</h4>
         <hr>
 
         <div class="owl-carousel owl-theme">
             <div class="item">
                 <div class="card border-0 font-rale mr-5" style="width: 18rem;">
-                    <h5 class="card-title font-size-16">Titulo del blog</h5>
-                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
-                    <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
-                    <a href="#" class="color-second text-left">Go somewhere</a>
-                </div>
-            </div>
-            <div class="item">
-                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
-                    <h5 class="card-title font-size-16">Titulo del blog</h5>
-                    <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
-                    <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
-                    <a href="#" class="color-second text-left">Go somewhere</a>
-                </div>
-            </div>
-            <div class="item">
-                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
-                    <h5 class="card-title font-size-16">Titulo del blog</h5>
-                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
-                    <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
-                    <a href="#" class="color-second text-left">Go somewhere</a>
+                    <h5 class="card-title font-size-16">Nueva coleccion</h5>
+                    <img src="./assets/ei.png" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">En Esmeralda lanzamos nuestra colección cápsula de edición 
+                              limitada. Joyas únicas y exclusivas para poder lucir en las próximas
+                               fiestas. ¿Lo mejor? Que cada una de ellas entraña una bonita historia
+                               digna de contar, y que no te puedes perder.</p>
+                    <a href="blog.html" class="color-second text-left">Ver el blog</a>
                 </div>
             </div>
         </div>
